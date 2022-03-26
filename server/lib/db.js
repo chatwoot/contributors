4c88621ed64ca4862caefe6f33d42337f729686d
Pranav Raj S <pranav@chatwoot.com>
2022-03-26 19:22:00 +0530

    chore: Move data to server directory

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 import { Low, JSONFile } from 'lowdb';
 
 // Use JSON file for storage
-const file = join(process.env.PWD, '../shared/db.json');
+const file = join(process.env.PWD, './data/db.json');
 const adapter = new JSONFile(file);
 const db = new Low(adapter);
 
