5ce3938a992c1f0debca3ed98d0b9bec64b170d9
Fayaz Ahmed <15716057+fayazara@users.noreply.github.com>
2022-05-10 17:17:07 +0530

    Import statement fix

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ import parseISO from 'date-fns/parseISO/index.js';
 import GithubClient from './lib/github/client.js';
 import db from './lib/db/index.js';
 
-import contributorsConfig from '../contributors.config';
+import contributorsConfig from '../contributors.config.js';
 
 const { GITHUB_ACCESS_TOKEN } = process.env;
 const githubClient = new GithubClient(GITHUB_ACCESS_TOKEN);
